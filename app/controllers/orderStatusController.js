ffb96cf232425a8144fd40a654aa87ff1cfb9b70
vinay kumar c k <vinaykumarck427@gmail.com>
2019-07-03 01:10:54 +0530

    add get orderStatuses route

--- app/controllers/orderStatusController.js
+++ app/controllers/orderStatusController.js
@@ -5,12 +5,27 @@ const _ = require('lodash')
 
 const OrderStatus = require('../models/orderStatus')
 const authenticationUser = require('../middlewares/authenticationUser')
+const authenticateUser = require('../middlewares/authenticateUser')
+
 
 router.post('/',authenticationUser,authenticateUser,(req,res) => {
           const orderStatus = new OrderStatus(req.body)
           orderStatus.save()
                     .then(statuses => {
                               res.json(statuses)
+                    })
+                    .catch(err => {
+                              res.send(err)
+                    })
+})
+
+router.get('/',(req,res) => {
+          const id = req.query.orderId
+          OrderStatus.find({
+                    order:id
+          })
+                    .then(orderStatus => {
+                              res.json(orderStatus)
                     })
                     .catch(err => {
                               res.send(err)
