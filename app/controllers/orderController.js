dbe1b6773dad38ce5c7aa6e8e5a04d6a70d1d4b6
vinay kumar c k <vinaykumarck427@gmail.com>
2019-07-03 00:17:53 +0530

    add get order route

--- app/controllers/orderController.js
+++ app/controllers/orderController.js
@@ -18,5 +18,34 @@ router.post('/',authenticationUser,(req,res) => {
           })
 })
 
+router.get('/',authenticationUser,(req,res) => {
+          const {user} = req
+          Order.find({
+                    user:user._id
+          })
+          .then(orders => {
+                    res.json(orders)
+          })
+          .catch(err => {
+                    res.send(err)
+          })
+})
+
+router.get('/:id',authenticationUser,(req,res) => {
+          const {user} = req
+          const id = req.params.id
+          Order.findOne({
+                    _id:id,
+                    user:user._id
+          }).populate('user',['_id','name']).populate('product',['_id','productPrice','description','productName','imgUrl']).populate('address',['street','city','pincode','landmark'])
+          .then(order => {
+                    res.json(_.pick(order, ['user', 'total', 'orderCode', 'address', 'orderLineItem', 'paymentMethod','createdAt']))
+          })
+          .catch(err => {
+                    res.send(err)
+          })
+})
+
+
 
 module.exports = router
