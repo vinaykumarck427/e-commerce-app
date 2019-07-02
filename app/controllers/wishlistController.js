deb25476667eefa5ebd5b0123150729fc2ed8a45
vinay kumar c k <vinaykumarck427@gmail.com>
2019-07-02 20:13:44 +0530

    add get wishlists route

--- app/controllers/wishlistController.js
+++ app/controllers/wishlistController.js
@@ -1,17 +1,34 @@
 const express = require('express')
+const router = express.Router()
 const Wishlist = require('../models/wishlist')
 const _ = require('lodash')
+const authenticationUser = require('../middlewares/authinticationUser')
 
 router.post('/',authenticationUser,(req,res) => {
           const {user} = req
           const wishlist = new Wishlist(req.body)
-          Wishlist.find({
-                    user:user._id
-          }).populate('product')
-          .then(wishlist => {
-                    res.send(_.pick(product, ['_id', 'productname', 'productprice', 'description', 'imgurl', 'category']))
+          wishlist.user = user._id
+          wishlist.save()
+          .then(wishlists => {
+                    res.json(wishlists)
           })
           .catch(err => {
                     res.send(err)
           })
 })
+
+router.get('/',authenticationUser,(req,res)=>{
+          const {user} = req
+          Wishlist.find({
+                    user:user._id
+          }).populate('user',['_id','name']).populate('product', ['_id', 'productname', 'productprice', 'description', 'imgurl', 'category'])
+          .then(wishlists => {
+                    console.log(wishlists)
+                    res.json(wishlists)
+          })
+          .then(err => {
+                    res.send(err)
+          })
+})
+// router.get()
+module.exports = router
