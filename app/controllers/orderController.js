e28cb3e304bd5afa21a25d9db1a7a09bcb975c9a
vinay kumar c k <vinaykumarck427@gmail.com>
2019-07-03 00:28:18 +0530

    add delete order route

--- app/controllers/orderController.js
+++ app/controllers/orderController.js
@@ -23,12 +23,12 @@ router.get('/',authenticationUser,(req,res) => {
           Order.find({
                     user:user._id
           })
-          .then(orders => {
-                    res.json(orders)
-          })
-          .catch(err => {
-                    res.send(err)
-          })
+                    .then(orders => {
+                              res.json(orders)
+                    })
+                    .catch(err => {
+                              res.send(err)
+                    })
 })
 
 router.get('/:id',authenticationUser,(req,res) => {
@@ -38,14 +38,42 @@ router.get('/:id',authenticationUser,(req,res) => {
                     _id:id,
                     user:user._id
           }).populate('user',['_id','name']).populate('product',['_id','productPrice','description','productName','imgUrl']).populate('address',['street','city','pincode','landmark'])
-          .then(order => {
-                    res.json(_.pick(order, ['user', 'total', 'orderCode', 'address', 'orderLineItem', 'paymentMethod','createdAt']))
-          })
-          .catch(err => {
-                    res.send(err)
-          })
+                    .then(order => {
+                              res.json(_.pick(order, ['user', 'total', 'orderCode', 'address', 'orderLineItem', 'paymentMethod','createdAt']))
+                    })
+                    .catch(err => {
+                              res.send(err)
+                    })
 })
 
+router.put('/:id',authenticationUser,(req,res) => {
+          const {user} = req
+          const id = req.params.id
+          Order.findOneAndUpdate({
+                    _id:id,
+                    user:user._id
+          }, { $set: req.body }, { new: true }).populate('user', ['_id', 'name']).populate('product', ['_id', 'productPrice', 'description', 'productName', 'imgUrl']).populate('address', ['street', 'city', 'pincode', 'landmark'])
+                    .then(order => {
+                              res.json(_.pick(order, ['user', 'total', 'orderCode', 'address', 'orderLineItem', 'paymentMethod', 'createdAt']))
+                    })
+                    .catch(err => {
+                              res.send(err)
+                    })
+})
 
+router.delete('/:id',authenticationUser,(req,res) => {
+          const { user } = req
+          const id = req.params.id
+          Order.findOneAndDelete({
+                    _id:id,
+                    user:user._id
+          })
+                    .then(order => {
+                              res.json(order)
+                    })
+                    .catch(err => {
+                              req.send(err)
+                    })
+})
 
 module.exports = router
