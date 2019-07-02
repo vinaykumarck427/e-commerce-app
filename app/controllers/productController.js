af756b66ff4a4c31a008952d5f32eba1a2fa783e
vinay kumar c k <vinaykumarck427@gmail.com>
2019-07-02 12:04:18 +0530

    add get all products route

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -3,6 +3,9 @@ const Product = require('../models/product')
 const router =  express.Router()
 const _ = require('lodash')
 
+const authenticationUser = require('../middlewares/authinticationUser')
+const authenticateUser = require('../middlewares/authenticateUser')
+
 router.post('/',authenticationUser,authenticateUser,(req,res) => {
           const body = req.body
           const product = new Product(body)
@@ -15,15 +18,15 @@ router.post('/',authenticationUser,authenticateUser,(req,res) => {
           })
 })
 
-// router.get('/',(req,res) => {
-//           Product.find()
-//           .then(products => {
-//                     res.json(products)
-//           })
-//           .catch(err => {
-//                     res.send(err)
-//           })
-// })
+router.get('/', authenticationUser, authenticateUser,(req,res) => {
+          Product.find()
+          .then(products => {
+                    res.json(products)
+          })
+          .catch(err => {
+                    res.send(err)
+          })
+})
 
 // router.
 
